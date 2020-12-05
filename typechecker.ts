9ebec7db53ba5c088965e0b05520deb5774090e5
Kevin Wilde <kevin.wilde@coursehero.com>
2020-12-04 20:39:48 -0800

    prefill switch statement in typechecker

--- typechecker.ts
+++ typechecker.ts
@@ -10,5 +10,32 @@ export type Type =
   | { tag: "TyArrow"; paramTypes: Type[]; returnType: Type };
 
 export function typeCheck(term: Term): Type {
-  throw new Error("TODO");
+  switch (term.tag) {
+    case "TmBool":
+      throw new Error("TODO");
+    case "TmInt":
+      throw new Error("TODO");
+    case "TmStr":
+      throw new Error("TODO");
+    case "TmVar":
+      throw new Error("TODO");
+    case "TmIf": {
+      // { tag: "TmIf"; cond: Term; then: Term; else: Term }
+      throw new Error("TODO");
+    }
+    case "TmLet": {
+      // { tag: "TmLet"; name: string; val: Term; body: Term }
+      throw new Error("TODO");
+    }
+    case "TmAbs": {
+      // { tag: "TmAbs"; params: { name: string; typeAnn: Type }[]; body: Term }
+      throw new Error("TODO");
+    }
+    case "TmApp": {
+      // { tag: "TmApp"; func: Term; args: Term[] }
+      throw new Error("TODO");
+    }
+    default:
+      return assertNever(term);
+  }
 }
